2085ae34e8afd487760a3b73fa2514f09deb4b22
ajinla3bo <46244753+ajinla3bo@users.noreply.github.com>
2019-03-26 09:26:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("559439686364299264")
 setInterval(function() {
-channel.send(`sub2ajinla3bo sub2ajinla3bo sub2ajinla3bo sub2ajinla3bo`);
+channel.send(`ok i will sub2ajinla3bo`);
 }, 30)
 })
 

0c8058a74bfa06c5add7d7a0d13799349716008f
ajinla3bo <46244753+ajinla3bo@users.noreply.github.com>
2019-03-26 10:25:36 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("559439686364299264")
+setInterval(function() {
+channel.send(`sub2ajinla3bo sub2ajinla3bo sub2ajinla3bo sub2ajinla3bo`);
+}, 30)
+})
+
+client.login('NTU2NzkxMDA0OTQzMDg5Njc1.D3laBg.yLc8-QfGX7x8Y4nBy_0WJPt0O2A');
